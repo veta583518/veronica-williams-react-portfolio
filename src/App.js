2a936c79596a11d447a68aedb3c39705d6b1423a
CoCo Chanel <veta583518@gmail.com>
2021-05-13 21:43:05 -0500

    updated App to include About and Nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import About from "./components/About";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
 
 function App() {
   return (
     <div>
-      <About></About>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
